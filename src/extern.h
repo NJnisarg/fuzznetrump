d17bcf1234ecff115ef1a88857ee45744060d22d
njnisarg <NJnisarg@gmail.com>
2020-07-11 20:01:57 +0530

    added baseline code for ICMP fuzzing

--- src/extern.h
+++ src/extern.h
@@ -2,6 +2,8 @@ int pkt_create_ipv4(void *, size_t, const struct sockaddr_in *,
     const struct sockaddr_in *);
 int pkt_create_udp4(void *, size_t, const struct sockaddr_in *,
     const struct sockaddr_in *);
+int pkt_create_icmp4(void *, size_t, const struct sockaddr_in *,
+    const struct sockaddr_in *);
 int makeaddr(struct sockaddr_in *addr, const char *name);
 int netcfg_rump_if_tun(const char *, const struct sockaddr_in *,
     const struct sockaddr_in *, const struct sockaddr_in *);
--- src/pkt_create.c
+++ src/pkt_create.c
@@ -7,10 +7,18 @@
 #include <netinet/in.h>
 #include <netinet/ip.h>
 #include <netinet/udp.h>
+#include <netinet/ip_icmp.h>
 
 #include "extern.h"
 
 #define IP_HDR_SIZE sizeof(struct ip)
+
+#define NUM_CODE_UNREACH 16
+#define NUM_CODE_REDIRECT 4
+#define NUM_CODE_ROUTERADVERT 2
+#define NUM_CODE_TIMXCEED 2
+#define NUM_CODE_PARAMPROB 3
+#define NUM_CODE_PHOTURIS 6
 
 
 static void
@@ -29,18 +37,37 @@ print_ipv4Pkt(const struct ip *iphdr){
     printf("Src Address: %#.8x\n", ntohl(iphdr->ip_src.s_addr));
     printf("Dst Address: %#.8x\n", ntohl(iphdr->ip_dst.s_addr));
 
-    printf("Extra data: %s\n",
-	(const char *)&iphdr->ip_dst + sizeof(iphdr->ip_dst));
+    /* printf("Extra data: %s\n",
+	(const char *)&iphdr->ip_dst + sizeof(iphdr->ip_dst)); */
 
     printf("========================================\n");
 
 }
 
-static void print_udpPkt(const struct udphdr *udp)
+static void 
+print_udpPkt(const struct udphdr *udp)
 {
     printf("========================================\n");
 
     printf("Source Port: %d\n", udp->uh_sport);
+    printf("Dest Port: %d\n", udp->uh_dport);
+    printf("Len: %d\n", udp->uh_ulen);
+    printf("Checksum: %d\n", udp->uh_sum);
+
+    printf("Extra data: %s\n",
+	(const char *)&udp->uh_sum + sizeof(udp->uh_sum));
+
+    printf("========================================\n");
+}
+
+static void 
+print_icmpPkt(const struct icmp *icp)
+{
+    printf("========================================\n");
+
+    printf("ICMP Type: %d\n", icp->icmp_type);
+    printf("ICMP Type: %d\n", icp->icmp_code);
+    printf("ICMP Type: %d\n", icp->icmp_cksum);
     printf("Dest Port: %d\n", udp->uh_dport);
     printf("Len: %d\n", udp->uh_ulen);
     printf("Checksum: %d\n", udp->uh_sum);
@@ -66,6 +93,51 @@ in_cksum(const void *data, size_t len)
         sum += (sum >> 16);
 
         return (uint16_t)~sum;
+}
+
+uint8_t
+getRandType(uint8_t type)
+{
+    // Return one of the ICMP types from 0 to 40
+    return (type % ICMP_NTYPES);
+}
+
+uint8_t
+getRandCode(uint8_t type, uint8_t code)
+{
+    uint8_t retCode = 0;
+    switch(type){
+        case ICMP_UNREACH:
+            retCode = code % NUM_CODE_UNREACH;
+            break;
+
+        case ICMP_REDIRECT:
+            retCode = code % NUM_CODE_REDIRECT;
+            break;
+        
+        case ICMP_ROUTERADVERT:
+            retCode = code % NUM_CODE_ROUTERADVERT;
+            retCode *= 16; // 0 or 16 expected
+            break;
+        
+        case ICMP_TIMXCEED:
+            retCode = code % NUM_CODE_TIMXCEED;
+            break;
+
+        case ICMP_PARAMPROB:
+            retCode = code % NUM_CODE_PARAMPROB;
+            break;
+
+        case ICMP_PHOTURIS:
+            retCode = code % NUM_CODE_PHOTURIS;
+            break;
+        
+        default:
+            retCode = 0;
+            break;
+    }
+
+    return retCode;
 }
 
 int
@@ -98,7 +170,7 @@ pkt_create_ipv4(void *buf, size_t buflen, const struct sockaddr_in *src,
     iphdr->ip_id = htons(0);
 
     // Calculate the correct checksum of IP Header
-    iphdr->ip_sum = in_cksum(iphdr, sizeof(struct ip));
+    iphdr->ip_sum = in_cksum(iphdr, IP_HDR_SIZE);
 
 	// Print the packet structure
 	print_ipv4Pkt(iphdr);
@@ -122,6 +194,27 @@ pkt_create_udp4(void *buf, size_t buflen, const struct sockaddr_in *src,
     udp->uh_sum = 0; // If we set this to zero the checksum is made optional and will be ignored
 
     print_udpPkt(udp);
+
+    return 0;
+}
+
+int 
+pkt_create_icmp4(void *buf, size_t buflen, const struct sockaddr_in *src,
+    const struct sockaddr_in *dst)
+{
+    int rv = pkt_create_ipv4(buf, buflen, src, dst);
+    if(rv == -1)
+    {
+        errno = ENOSPC;
+		return rv;
+    }
+
+    struct icmp *icp = (struct icmp *)(buf + IP_HDR_SIZE);
+    icp->icmp_cksum = in_cksum(icp, buflen - IP_HDR_SIZE); // Checksum for the entire ICMP packet
+    icp->icmp_type = getRandType(icp->icmp_type);
+    icp->icmp_code = getRandCode(icp->icmp_type, icp->icmp_code);
+
+    print_icmpPkt(icp);
 
     return 0;
 }
